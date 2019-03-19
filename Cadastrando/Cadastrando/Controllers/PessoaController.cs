ee828a82d48a8014f16d1d9e12471ed5a9d58666
Unknown <mateus.amaral@fatec.sp.gov.br>
2019-03-18 21:45:04 -0300

    Criando Arquitetura + Get All de Pessoas

---
+++ Cadastrando/Cadastrando/Controllers/PessoaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Negocio;
+
+namespace Cadastrando.Controllers
+{
+    [Route("api/Pessoa")]// Rota da  url
+
+    public class PessoaController : ControllerBase
+    {
+        PessoaNegocio pessoaNegocio = new PessoaNegocio();
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(pessoaNegocio.Selecionar());
+        } 
+
+    }
+}
---
+++ Cadastrando/Model/PessoaModelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+   public class PessoaModelo
+    {
+        public int id { get; set; }//SET - Colocando um valor aqui; GET - Pegando valor aqui atribuido
+        public string nome { get; set; }
+        public string usuario { get; set; }
+    }
+}
